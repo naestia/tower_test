fa3213c3d75ab9ee8d8b2bf4c199d83f8bbb7be6
Viktor Berg <viktor.david.berg@gmail.com>
2024-04-17 13:57:35 +0200

    Adding stuff

---
+++ config.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    region = "eu-north-1"
+    bucket = "tower-test-tf-backend"
+    key    = "core/terraform.tfstate"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.45"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "eu-north-1"
+  profile = "sso-ductus"
+}
