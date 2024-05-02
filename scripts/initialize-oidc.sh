83e291b93b14a0ae42f3cabfcd8ddcc9975271a8
Viktor Berg <viktor.david.berg@gmail.com>
2024-05-02 15:17:29 +0200

    Adding stuff

--- scripts/initialize-oidc.sh
+++ scripts/initialize-oidc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir ../web-identity && mkdir ../artifacts
-export AWS_WEB_IDENTITY_TOKEN_FILE=../web-identity/web-identity-token
-echo $BITBUCKET_STEP_OIDC_TOKEN > ../web-identity/web-identity-token
+mkdir ./web-identity && mkdir ./artifacts
+export AWS_WEB_IDENTITY_TOKEN_FILE=./web-identity/web-identity-token
+echo $BITBUCKET_STEP_OIDC_TOKEN > ./web-identity/web-identity-token
