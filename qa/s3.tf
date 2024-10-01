1ca87c450f9782e4c2e2c8441d6a7889f233cf2a
Viktor Berg <90246244+naestia@users.noreply.github.com>
2024-10-01 12:20:05 +0200

    Update s3.tf

--- qa/s3.tf
+++ qa/s3.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
   bucket = aws_s3_bucket.bucket.id
   versioning_configuration {
-    status = enabled
+    status = "Enabled"
   }
 }
 
