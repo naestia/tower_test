74c7f4b8929d4ffba1e6ebb8d9685bd9b20faa09
Viktor Berg <viktor.david.berg@gmail.com>
2024-10-01 14:46:20 +0200

    Adding stuff

--- qa/config.tf
+++ qa/config.tf
@@ -1,18 +1,14 @@
-# terraform {
-#   backend "s3" {
-#     region = "eu-north-1"
-#     bucket = "tower-qa-tf-backend"
-#     key    = "core/terraform.tfstate"
-#   }
+terraform {
+  backend "s3" {
+    region = "eu-north-1"
+    bucket = "tower-qa-tf-backend"
+    key    = "core/terraform.tfstate"
+  }
 
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.45"
-#     }
-#   }
-# }
-
-provider "aws" {
-  region  = "eu-north-1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.45"
+    }
+  }
 }
---
+++ qa/main.tf
@@ -0,0 +1,3 @@
+resource "aws_vpc" "vpc_1" {
+  cidr_block = "10.0.0.13"
+}
