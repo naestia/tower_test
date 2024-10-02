cfe5dd0bae7ba4d277d2b3faf2fc06ed2972f57a
Viktor Berg <viktor.david.berg@gmail.com>
2024-10-02 13:26:12 +0200

    Update actions

--- qa/config.tf
+++ qa/config.tf
@@ -12,23 +12,3 @@ terraform {
     }
   }
 }
-
-data "aws_s3_bucket" "selected" {
-  bucket = "tower-qa-tf-backend"
-}
-
-resource "aws_kms_key" "mykey" {
-    description = "Key"
-    deletion_window_in_days = 10
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-    bucket = data.aws_s3_bucket.selected.id
-
-    rule {
-        apply_server_side_encryption_by_default {
-          kms_master_key_id = aws_kms_key.mykey.arn
-          sse_algorithm = "aws:kms"
-        }
-    }
-}
