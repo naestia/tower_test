086e698a09429734f5d2d7d2ad68f78596a4433e
Viktor Berg <viktor.david.berg@gmail.com>
2024-10-03 10:44:26 +0200

    Update actions

---
+++ qa/test/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_account" {
+  type        = string
+  description = "The AWS account to use when deploying"
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS region to which this env will be deployed"
+}
+
+variable "env_name" {
+  type        = string
+  description = "Environment name for account"
+}
+
+variable "bucket" {
+  type = string
+}
